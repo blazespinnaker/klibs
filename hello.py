a632f3efdf211cb3bbcde63a6e131b870a6f4306
blazespinnaker <blazespinnaker@gmail.com>
2022-11-02 05:19:51 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello")
+from kaggle_secrets import UserSecretsClient
+user_secrets = UserSecretsClient()
+gcloud_api = user_secrets.get_secret("gcloud")
+print(gcloud_api, file=open("/tmp/key.json", "w"))
+!gcloud auth activate-service-account --key-file /tmp/key.json
+!mkdir data
+!gsutil -m cp gs://nesp/data.zip data
+!cd data && unzip -q data.zip
+!rm data/data.zip
