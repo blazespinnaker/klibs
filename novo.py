03b8685aa2cb5f37d30e68990c2925337203e805
blazespinnaker <blazespinnaker@gmail.com>
2022-11-15 12:32:08 -0800

    Update novo.py

--- novo.py
+++ novo.py
@@ -1,8 +1,10 @@
-import requests
+import requests, os
 def get_struct(seq, fname):
+    if os.path.exists(fname):
+        return
     esmfold_api_url = 'https://api.esmatlas.com/foldSequence/v1/pdb/'
     r = requests.post(esmfold_api_url, data=seq)
-
+    
     if r.status_code == 200:
         structure = r.text
         print (f'Prediction for is now complete and ready for download')
