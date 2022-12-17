46df61b562bef659d610c874dc0b035e016b1d91
blazespinnaker <blazespinnaker@gmail.com>
2022-12-16 23:26:59 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import io
+from setuptools import setup, find_packages
+setup(
+    name='klib',
+    version='1.7.0',
+    description='utils for k',
+    author='blaze',
+    author_email='blaze@gmail.com',
+    url='https://github.com/blazespinnaker/klibs',
+    license='MIT',
+    install_requires=['numpy','pandas'],
+    py_modules=['klib'],
+    packages=find_packages(),
+    long_description_content_type='text/markdown',
+    long_description=io.open('README.md', encoding='utf-8').read()
+)
