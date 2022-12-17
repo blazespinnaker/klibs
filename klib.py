e081d50b3e7099a979e8734a9eed3d6aeaf7aebc
blazespinnaker <blazespinnaker@gmail.com>
2022-12-16 23:40:15 -0800

    Delete klib.py

--- klib.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd, numpy as np
-import dtale
-print("!pip install dtale")
-print("import pandas as pd, numpy as np")
-def explore(df):
-  '''
-  explore(df) - uses dtale to show lots of info about df
-  '''
-  dtale.show(df)
